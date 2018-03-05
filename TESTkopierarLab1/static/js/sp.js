e097cb6792ebd2e6ea1705cd948abd9e02ad312f
ElonOlsson <olssonelon@gmail.com>
2018-03-05 10:32:33 +0100

    mostly comments

--- TESTkopierarLab1/static/js/sp.js
+++ TESTkopierarLab1/static/js/sp.js
@@ -1,8 +1,3 @@
-/*
-  Created: Jan 14 2018
-  Author: Kahin Akram Hassan
-*/
-
 function sp(data){
 
     this.data = data;
@@ -33,7 +28,6 @@ function sp(data){
         .append("g")
         .attr("transform","translate(" + margin.left + "," + margin.top + ")");
     
-        /* ~~ Task 3 Add the x and y Axis and title  ~~ */
 		//xAxis
 	svg.append("g")
 		.attr("transform", "translate(0, " + height +")")
@@ -54,10 +48,6 @@ function sp(data){
 		.style("text-anchor", "middle")
 		.text("Procent röster");
 			
-		
-
-        /* ~~ Task 4 Add the scatter dots. ~~ */
-	
 	/*var circles = svg.selectAll("dots").data(data)
 		.enter().append("circle")
 		.attr("class", "circle")		
@@ -69,77 +59,71 @@ function sp(data){
         .style("stroke-width", 5); */
         
           // append the rectangles for the bar chart
-     var bar = svg.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function(d) { return xScale(d.parti); })
-            .attr("width", xScale.bandwidth())
-            .attr("y", function(d) { return yScale(d.y2014); })
-            .attr("height", function(d) { return height - yScale(d.y2014); });
+    var bar = svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return xScale(d.parti); })
+        .attr("width", xScale.bandwidth())
+        .attr("y", function(d) { return yScale(d.y2014); })
+        .attr("height", function(d) { return height - yScale(d.y2014); });
+    
+        bar.append("text")
+        .attr("class", "value")
+        .attr("x", xScale.bandwidth()/2)
+        .attr("text-anchor", "end")
+        .style('fill', 'white')
+        .text(function(d){return d.value + "%";})
+/*
+    //highlightBrushedCircles function
+    function highlightBrushedCircles() {
+        if (d3.event.selection != null) {
+            // revert circles to initial style
+            circles.attr("class", "non_brushed");
+            var brush_coords = d3.brushSelection(this);
+            // style brushed circles
+            circles.filter(function (){
+                var cx = d3.select(this).attr("cx");
+                var cy = d3.select(this).attr("cy");
+                return isBrushed(brush_coords, cx, cy);
+            })
+            .attr("class", "brushed");
+            var d_brushed =  d3.selectAll(".brushed").data();
 
-        /* ~~ Task 5 create the brush variable and call highlightBrushedCircles() ~~ */
-		
-	var brush = d3.brush().on("start brush end", highlightBrushedCircles);
-	
-	svg.append("g")
-		.attr("class", "brush")
-		.call(brush);
-		
-	 
-				 //return value.every(function(v){
-			//? null : "red" 
-				 
+*/
+            /* ~~~ Call pc or/and map function to filter ~~~ */
+            
+            //console.log(d_brushed);
+            
+            
+            //var array = [];
+            //array.push(d_brushed);
+/*            map.selectCountry(d_brushed);
+            
 
-         //highlightBrushedCircles function
-         function highlightBrushedCircles() {
-             if (d3.event.selection != null) {
-                 // revert circles to initial style
-                 circles.attr("class", "non_brushed");
-                 var brush_coords = d3.brushSelection(this);
-                 // style brushed circles
-                   circles.filter(function (){
-                            var cx = d3.select(this).attr("cx");
-                            var cy = d3.select(this).attr("cy");
-                            return isBrushed(brush_coords, cx, cy);
-                  })
-                  .attr("class", "brushed");
-                   var d_brushed =  d3.selectAll(".brushed").data();
+        }
+    }//highlightBrushedCircles
+    function isBrushed(brush_coords, cx, cy) {
+        var x0 = brush_coords[0][0],
+            x1 = brush_coords[1][0],
+            y0 = brush_coords[0][1],
+            y1 = brush_coords[1][1];
+        return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;
+    }//isBrushed
 
+*/
 
-                   /* ~~~ Call pc or/and map function to filter ~~~ */
-				   
-					//console.log(d_brushed);
-					
-					
-					//var array = [];
-					//array.push(d_brushed);
-					map.selectCountry(d_brushed);
-				   
+    //Select all the dots filtered
+    this.selectDots = function(value){
+        
+        var dots = d3.selectAll('.non_brushed');
+        dots.style('stroke', function(d){
+            
+            return value.properties.name == d.Country ? "blue" : null 
 
-             }
-         }//highlightBrushedCircles
-         function isBrushed(brush_coords, cx, cy) {
-              var x0 = brush_coords[0][0],
-                  x1 = brush_coords[1][0],
-                  y0 = brush_coords[0][1],
-                  y1 = brush_coords[1][1];
-             return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;
-         }//isBrushed
-
-
-
-         //Select all the dots filtered
-         this.selectDots = function(value){
-			 
-				var dots = d3.selectAll('.non_brushed');
-				dots.style('stroke', function(d){
-					
-						return value.properties.name == d.Country ? "blue" : null 
-		
-				});
-				
-         };
+        });
+        
+    };
 
 
 }//End
